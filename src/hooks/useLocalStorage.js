55ff4d27abbdd4e55cb4e3247a713237775b366e
Clear008 <wissal-exo@hotmail.com>
2025-01-29 18:17:20 +0100

    ajout dh Hook useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage(key, initialValue) {
+  // Récupérer la valeur initiale du localStorage ou utiliser initialValue
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  // Mettre à jour le localStorage quand la valeur change
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(storedValue));
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+}
