e2c89831a93c88975b844a8903cbd6b0a4db5f13
Clear008 <wissal-exo@hotmail.com>
2025-01-29 17:46:52 +0100

    deplacement du codeBlock translation dans un fichier translations.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,9 @@ import ProductList from './components/ProductList';
 import ProductSearch from './components/ProductSearch';
 import ThemeToggle from './components/ThemeToggle';
 import LanguageSelector from './components/LanguageSelector';
+import { translations } from './utils/translations';
 
-export const translations = {
-  fr: {
-    title: "Catalogue de Produits",
-    search: "Rechercher un produit...",
-    loading: "Chargement...",
-    price: "Prix:",
-    darkMode: "Mode Sombre",
-    lightMode: "Mode Clair",
-    results: "Résultats pour",
-    productsFound: "produits trouvés"
-  },
-  en: {
-    title: "Product Catalog",
-    search: "Search for a product...",
-    loading: "Loading...",
-    price: "Price:",
-    darkMode: "Dark Mode",
-    lightMode: "Light Mode",
-    results: "Results for",
-    productsFound: "products found"
-  }
-};
+
 
 export const LanguageContext = createContext();
 export const ThemeContext = createContext();
@@ -46,7 +26,7 @@ const App = () => {
       <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
         <div className={`container ${isDarkTheme ? 'bg-dark text-light' : 'bg-light'}`}>
           <header className="my-4">
-            <h1 className="text-center">{translations[language].title}</h1>
+          <h1 className="text-center">{translations[language].title}</h1>
             <div className="d-flex justify-content-end gap-2">
               <ThemeToggle />
               <LanguageSelector />
---
+++ src/utils/translations.js
@@ -0,0 +1,23 @@
+export const translations = {
+  
+    fr: {
+    title: "Catalogue de Produits",
+    search: "Rechercher un produit...",
+    loading: "Chargement...",
+    price: "Prix:",
+    darkMode: "Mode Sombre",
+    lightMode: "Mode Clair",
+    results: "Résultats pour",
+    productsFound: "produits trouvés"
+  },
+  en: {
+    title: "Product Catalog",
+    search: "Search for a product...",
+    loading: "Loading...",
+    price: "Price:",
+    darkMode: "Dark Mode",
+    lightMode: "Light Mode",
+    results: "Results for",
+    productsFound: "products found"
+  }
+};
